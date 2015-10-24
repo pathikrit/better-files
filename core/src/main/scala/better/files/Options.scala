3ecba18c63f33ce7fc386d530e2bcd3836eead43
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-10-24 13:30:42 -0700

    #27: Rename Defaults to Options

--- core/src/main/scala/better/files/Options.scala
+++ core/src/main/scala/better/files/Options.scala
@@ -4,9 +4,9 @@ import java.nio.file.{LinkOption, StandardWatchEventKinds}
 import java.nio.file.attribute.FileAttribute
 
 /**
- * Sensible defaults that APIs use unless overridden
+ * Container default settings for various options
  */
-object Defaults {
+object Options {
   val delimiters = " \t\n\r\f"
 
   val events = Seq(StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE)
