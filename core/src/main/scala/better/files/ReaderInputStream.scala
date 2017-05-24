86d323bd0da60a675a4a7d9bc58dc0459e398015
pathikrit <pathikritbhowmick@msn.com>
2017-05-24 19:13:49 -0400

    tailrec ReaderInputStream.scala

--- core/src/main/scala/better/files/ReaderInputStream.scala
+++ core/src/main/scala/better/files/ReaderInputStream.scala
@@ -2,27 +2,39 @@ package better.files
 
 import java.io.{InputStream, Reader}
 import java.nio.{ByteBuffer, CharBuffer}
-import java.nio.charset.{Charset, CoderResult, CodingErrorAction}
+import java.nio.charset.{Charset, CharsetEncoder, CoderResult, CodingErrorAction}
+
+import scala.annotation.tailrec
 
 /**
   * Code ported from Java to Scala:
   * https://github.com/apache/commons-io/blob/c0eb48f7e83987c5ed112b82f0d651aff5149ae4/src/main/java/org/apache/commons/io/input/ReaderInputStream.java
   */
-class ReaderInputStream(reader: Reader, bufferSize: Int = defaultBufferSize)(implicit charset: Charset = File.defaultCharset) extends InputStream {
-  private[this] val encoder = charset.newEncoder().onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE)
+class ReaderInputStream(reader: Reader, encoder: CharsetEncoder, bufferSize: Int) extends InputStream {
+
+  def this(reader: Reader, bufferSize: Int = defaultBufferSize)(implicit charset: Charset = File.defaultCharset) =
+    this(reader = reader, encoder = charset.newEncoder().onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE), bufferSize = bufferSize)
+
   private[this] var endOfInput = false
-  private[this] val encoderIn = CharBuffer.allocate(bufferSize)
-  private[this] val encoderOut = ByteBuffer.allocate(128)
-  private[this] var lastCoderResult: CoderResult = null
 
-  encoderIn.flip()
-  encoderOut.flip()
+  /**
+    * CharBuffer used as input for the decoder. It should be reasonably
+    * large as we read data from the underlying Reader into this buffer.
+    */
+  private[this] val encoderIn = CharBuffer.allocate(bufferSize).flip().asInstanceOf[CharBuffer]
 
+  /**
+    * ByteBuffer used as output for the decoder. This buffer can be small
+    * as it is only used to transfer data from the decoder to the buffer provided by the caller.
+    */
+  private[this] val encoderOut = ByteBuffer.allocate(128).flip().asInstanceOf[ByteBuffer]
 
-  private def fillBuffer() = {
-    if (!endOfInput && (lastCoderResult == null || lastCoderResult.isUnderflow)) {
-      encoderIn.compact
-      val position = encoderIn.position
+  private[this] var lastCoderResult = CoderResult.UNDERFLOW
+
+  private[this] def fillBuffer() = {
+    assert(!endOfInput)
+    if (lastCoderResult.isUnderflow) {
+      val position = encoderIn.compact().position
       // We don't use Reader#read(CharBuffer) here because it is more efficient
       // to write directly to the underlying char array (the default implementation
       // copies data to a temporary char array).
@@ -30,43 +42,41 @@ class ReaderInputStream(reader: Reader, bufferSize: Int = defaultBufferSize)(imp
       if (c == EOF) endOfInput = true else encoderIn.position(position + c)
       encoderIn.flip
     }
-    encoderOut.compact
-    lastCoderResult = encoder.encode(encoderIn, encoderOut, endOfInput)
+    lastCoderResult = encoder.encode(encoderIn, encoderOut.compact(), endOfInput)
     encoderOut.flip
   }
 
-  override def read(b: Array[Byte], _off: Int, _len: Int): Int = {
-    var len = _len
-    var off = _off
-    if (b == null) throw new NullPointerException("Byte array must not be null")
+  override def read(b: Array[Byte], off: Int, len: Int) = {
     if (len < 0 || off < 0 || (off + len) > b.length) throw new IndexOutOfBoundsException("Array Size=" + b.length + ", offset=" + off + ", length=" + len)
-    var read = 0
-    if (len == 0) return 0 // Always return 0 if len == 0
-    while (len > 0) {
-      if (encoderOut.hasRemaining) {
-        val c = Math.min(encoderOut.remaining, len)
-        encoderOut.get(b, off, c)
-        off += c
-        len -= c
-        read += c
-      }
-      else {
-        fillBuffer()
-        if (endOfInput && !encoderOut.hasRemaining) {
-          len = 0
+    if (len == 0) {
+      0 // Always return 0 if len == 0
+    } else {
+      var read = 0
+      @tailrec def loop(off: Int, len: Int): Unit = if (len > 0) {
+        if (encoderOut.hasRemaining) {
+          val c = encoderOut.remaining min len
+          encoderOut.get(b, off, c)
+          read += c
+          loop(off + c, len - c)
+        } else if (!endOfInput) {
+          fillBuffer()
+          loop(off, len)
         }
       }
+      loop(off, len)
+      if (read == 0 && endOfInput) EOF else read
     }
-    if (read == 0 && endOfInput) EOF else read
   }
 
-  override def read(): Int = {
-    while (true) {
-      if (encoderOut.hasRemaining) return encoderOut.get & 0xFF
+  @tailrec final override def read() = {
+    if (encoderOut.hasRemaining) {
+      encoderOut.get & 0xFF
+    } else if (endOfInput) {
+      EOF
+    } else {
       fillBuffer()
-      if (endOfInput && !encoderOut.hasRemaining) return EOF
+      read()
     }
-    -1
   }
 
   override def close() = reader.close()
