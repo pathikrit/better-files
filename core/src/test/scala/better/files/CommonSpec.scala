9546c6f8c74649ff3db06a7f0fad5bcd3de1407b
Alex Hvostov <argv-minus-one@users.noreply.github.com>
2017-08-27 18:59:00 -0700

    Fix typo in CommonSpec#isUnixOS, by just using scala.util.Properties.isMac and .isLinux instead.

--- core/src/test/scala/better/files/CommonSpec.scala
+++ core/src/test/scala/better/files/CommonSpec.scala
@@ -4,14 +4,12 @@ import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.Properties.{isLinux, isMac}
 
 trait CommonSpec extends FlatSpec with BeforeAndAfterEach with Matchers {
   val isCI = sys.env.get("CI").exists(_.toBoolean)
 
-  val isUnixOS = sys.props.get("os.name") match {
-    case Some("Linux" | "MaxOS") => true
-    case _ => false
-  }
+  val isUnixOS = isLinux || isMac
 
   def sleep(t: FiniteDuration = 2 second) = Thread.sleep(t.toMillis)
 }
