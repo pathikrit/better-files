b7c56cff7bcb5cbdfe90eaf183c2508e41c59e1a
Alex Hvostov <argv-minus-one@users.noreply.github.com>
2017-08-28 18:37:59 -0700

    Add more complete exception handling to ManagedResource.
    It now catches all exceptions, and uses the suppressed exception facility, like Java try-with-resources does.
    
    This fixes #173.

--- core/src/main/scala/better/files/ManagedResource.scala
+++ core/src/main/scala/better/files/ManagedResource.scala
@@ -3,6 +3,7 @@ package better.files
 import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.util.Try
+import scala.util.control.NonFatal
 
 /**
   * A typeclass to denote a disposable resource
@@ -32,16 +33,39 @@ object Disposable {
 
 class ManagedResource[A](resource: A)(implicit disposer: Disposable[A]) {
   private[this] val isDisposed = new AtomicBoolean(false)
-  private[this] def disposeOnce() = if (!isDisposed.getAndSet(true)) disposer.disposeSilently(resource)
+  private[this] def disposeOnce() = if (!isDisposed.getAndSet(true)) disposer.dispose(resource)
 
   def foreach[U](f: A => U): Unit = {
     val _ = map(f)
   }
 
   def map[B](f: A => B): B = {
-    val result = Try(f(resource))
-    disposeOnce()
-    result.get
+    // Avoid using Option here. If an OutOfMemoryError is caught, allocating an Option may cause another one.
+    var e1: Throwable = null
+
+    // This is the Scala equivalent of how javac compiles try-with-resources, except that fatal exceptions while disposing take precedence over exceptions thrown by the provided function.
+    try
+      f(resource)
+    catch { case e: Throwable =>
+      e1 = e
+      throw e
+    }
+    finally {
+      if (e1 ne null) {
+        try
+          disposeOnce()
+        catch { case e2: Throwable =>
+          if (NonFatal(e2))
+            e1.addSuppressed(e2)
+          else {
+            e2.addSuppressed(e1)
+            throw e2
+          }
+        }
+      }
+      else
+        disposeOnce()
+    }
   }
 
   def withFilter(f: A => Boolean): this.type = {
@@ -60,9 +84,25 @@ class ManagedResource[A](resource: A)(implicit disposer: Disposable[A]) {
   def flatMap[B](f: A => Iterator[B]): Iterator[B] = {
     val it = f(resource)
     it withHasNext {
-      val result = Try(it.hasNext)
-      if (!result.getOrElse(false)) disposeOnce()
-      result.get
+      try {
+        val result = it.hasNext
+        if (!result) disposeOnce()
+        result
+      }
+      catch { case e1: Throwable =>
+        try
+          disposeOnce()
+        catch { case e2: Throwable =>
+          if (NonFatal(e2))
+            e1.addSuppressed(e2)
+          else {
+            e2.addSuppressed(e1)
+            throw e2
+          }
+        }
+
+        throw e1
+      }
     }
   }
 }
