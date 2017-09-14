pathikrit/better-files

commit 6c929205a13e4df4246304a5d554a19afa8b6b62

Fix #171: Work around for http://bugs.java.com/view_bug.do?bug_id=8032220

core/src/main/scala/better/files/File.scala
@@ -193,12 +193,20 @@ class File private(val path: Path)(implicit val fileSystem: FileSystem = path.ge
     loadBytes
 
   def createDirectory()(implicit attributes: File.Attributes = File.Attributes.default): this.type = {
-    Files.createDirectory(path, attributes: _*)
+    try {
+      Files.createDirectory(path, attributes: _*)
+    } catch {
+      case _: FileAlreadyExistsException => // We don't really care if it exists already
+    }
     this
   }
 
   def createDirectories()(implicit attributes: File.Attributes = File.Attributes.default): this.type = {
-    Files.createDirectories(path, attributes: _*)
+    try {
+      Files.createDirectories(path, attributes: _*)
+    } catch {
+      case _: FileAlreadyExistsException => // We don't really care if it exists already
+    }
     this
   }
 

core/src/test/scala/better/files/FileSpec.scala
@@ -291,8 +291,11 @@ class FileSpec extends CommonSpec {
   }
 
   it should "create if not exist" in {
-    def doTest(dir: File) = repeat(5) {
-      (dir / "file.txt").createIfNotExists(createParents = true)
+    def doTest(dir: File) = {
+      dir.delete().symbolicLinkTo(File.temp)
+      repeat(5) {
+        (dir / "file.txt").createIfNotExists(createParents = true)
+      }
     }
     noException should be thrownBy File.temporaryDirectory().foreach(doTest)
   }

commit acecafb3758fd620c13393df140a9ae76165ffe7

Investigate breakage from https://github.com/pathikrit/better-files/commit/6c929205a13e4df4246304a5d554a19afa8b6b62

core/src/main/scala/better/files/File.scala
@@ -193,20 +193,22 @@ class File private(val path: Path)(implicit val fileSystem: FileSystem = path.ge
     loadBytes
 
   def createDirectory()(implicit attributes: File.Attributes = File.Attributes.default): this.type = {
-    try {
-      Files.createDirectory(path, attributes: _*)
-    } catch {
-      case _: FileAlreadyExistsException => // We don't really care if it exists already
-    }
+    Files.createDirectory(path, attributes: _*)
+//    try {
+//      Files.createDirectory(path, attributes: _*)
+//    } catch {
+//      case _: FileAlreadyExistsException => // We don't really care if it exists already
+//    }
     this
   }
 
   def createDirectories()(implicit attributes: File.Attributes = File.Attributes.default): this.type = {
-    try {
-      Files.createDirectories(path, attributes: _*)
-    } catch {
-      case _: FileAlreadyExistsException => // We don't really care if it exists already
-    }
+    Files.createDirectories(path, attributes: _*)
+//    try {
+//      Files.createDirectories(path, attributes: _*)
+//    } catch {
+//      case _: FileAlreadyExistsException => // We don't really care if it exists already
+//    }
     this
   }
 

akka/src/test/scala/better/files/FileWatcherSpec.scala
@@ -44,10 +44,9 @@ class FileWatcherSpec extends CommonSpec {
         sleep()
       }
 
-      /* TODO: Uncomment this!
       doIO("a/b/c.txt got modified") {
         (dir / "a" / "b" / "c.txt").writeText("Hello world")
-      }*/
+      }
       doIO("a/b got deleted", "a/b/c.txt got deleted") {
         rm(dir / "a" / "b")
       }