3cdc419bb3a1096f6c7cc8e3a0076aa7e849e87f
pathikrit <pathikritbhowmick@msn.com>
2018-05-21 23:18:08 -0400

    Simplify resource test

--- core/src/test/scala/better/files/ResourceSpec.scala
+++ core/src/test/scala/better/files/ResourceSpec.scala
@@ -20,14 +20,11 @@ final class ResourceSpec extends CommonSpec {
   it can "look up from a specified class loader" in {
     val clURL = new URL(Resource.my.getUrl("ResourceSpec.class"), "../")
     assert(clURL.toExternalForm endsWith "/")
-    val cl = new URLClassLoader(Array(clURL))
-
-    assert(Resource.from(cl).getAsString(testFileFromCL) startsWith testFileText)
+    assert(Resource.from(new URLClassLoader(Array(clURL))).getAsString(testFileFromCL) startsWith testFileText)
   }
 
   it can "look up from the call site" in {
     assert(Resource.my.asStream(testFileRel).get.asString() startsWith testFileText)
-
     // This tests that Resource.my uses the correct call site when called from outside the better.files package.
     assert((new ResourceSpecHelper).openTestStream().asString() startsWith altTestFileText)
   }
@@ -38,16 +35,14 @@ final class ResourceSpec extends CommonSpec {
   }
 
   it can "look up from a java.lang.Class" in {
-    def testClass: Class[_] = Class forName "better.files.File"
-
-    assert(Resource.at(testClass).getAsString(testFileRel) startsWith testFileText)
+    assert(Resource.at(Class.forName("better.files.File")).getAsString(testFileRel) startsWith testFileText)
   }
 
   it can "look up a file in another package" in {
     assert(Resource.at[ResourceSpecHelper].getAsString(testFileAltRel) startsWith altTestFileText)
   }
 
-  "Resource.at" should "require a concrete type" in {
+  it should "require a concrete type" in {
     """def foo[T] = better.files.Resource.at[T].asStream("foo")""" shouldNot typeCheck
   }
 }
