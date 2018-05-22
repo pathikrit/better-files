9c0dfbc7fb675489942333214e77a86bc0bbd98c
pathikrit <pathikritbhowmick@msn.com>
2018-05-21 22:21:11 -0400

    Rename ResourceLoader to Resource.scala

--- core/src/main/scala/better/files/Resource.scala
+++ core/src/main/scala/better/files/Resource.scala
@@ -8,7 +8,7 @@ import scala.reflect.macros.{ReificationException, blackbox}
 /**
   * Finds and loads [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) class resources]] or [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html#getResource(java.lang.String) class loader resources]].
   *
-  * The default implementation of this trait is the [[Resource]] object, which looks up resources using the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#currentThread() current thread]]'s [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#getContextClassLoader() context class loader]]. The Resource object also offers several other ResourceLoader implementations, through its methods `at`, `from`, and `my`. `at` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]], `from` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html ClassLoader]], and `my` searches from the class, trait, or object surrounding the call.
+  * The default implementation of this trait is the [[Resource]] object, which looks up resources using the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#currentThread() current thread]]'s [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#getContextClassLoader() context class loader]]. The Resource object also offers several other Resource implementations, through its methods `at`, `from`, and `my`. `at` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]], `from` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html ClassLoader]], and `my` searches from the class, trait, or object surrounding the call.
   *
   * @example {{{
   *          // Look up the config.properties file for this class or object.
@@ -23,7 +23,7 @@ import scala.reflect.macros.{ReificationException, blackbox}
   * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]
   * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html#getResource(java.lang.String) ClassLoader#getResource]]
   */
-trait ResourceLoader {
+trait Resource {
 
   /**
     * Look up a resource by name, and open an [[https://docs.oracle.com/javase/10/docs/api/java/io/InputStream.html InputStream]] for reading it.
@@ -50,16 +50,16 @@ trait ResourceLoader {
 }
 
 /**
-  * Implementations of [[ResourceLoader]].
+  * Implementations of [[Resource]].
   *
-  * This object itself is a ResourceLoader uses the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#currentThread() current thread]]'s [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#getContextClassLoader() context class loader]]. It also creates ResourceLoaders with different lookup behavior, using the methods `at`, `from`, and `my`. `at` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]], `from` searches from a different [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html ClassLoader]], and `my` searches from the class, trait, or object surrounding the call.
+  * This object itself is a Resource uses the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#currentThread() current thread]]'s [[https://docs.oracle.com/javase/10/docs/api/java/lang/Thread.html#getContextClassLoader() context class loader]]. It also creates Resources with different lookup behavior, using the methods `at`, `from`, and `my`. `at` searches from a [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]], `from` searches from a different [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html ClassLoader]], and `my` searches from the class, trait, or object surrounding the call.
   *
-  * @see [[ResourceLoader]]
+  * @see [[Resource]]
   * @see [[https://stackoverflow.com/questions/676250/different-ways-of-loading-a-file-as-an-inputstream Different ways of loading a file as an InputStream]]
   * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]
   * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html#getResource(java.lang.String) ClassLoader#getResource]]
   */
-object Resource extends ResourceLoader {
+object Resource extends Resource {
 
   override def url(name: String): Option[URL] =
     from(Thread.currentThread.getContextClassLoader).url(name)
@@ -67,22 +67,22 @@ object Resource extends ResourceLoader {
   /**
     * Look up class resource files.
     *
-    * This ResourceLoader looks up resources relative to the JVM class file for `T`, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `com.example.ExampleClass` is given for `T`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
+    * This Resource looks up resources relative to the JVM class file for `T`, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `com.example.ExampleClass` is given for `T`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
     *
     * If you want to look up resource files relative to the call site instead (that is, you want a class to look up one of its own resources), use the `my` method instead.
     *
     * @example {{{ Resource.at[YourClass].url("config.properties") }}}
     * @tparam T The class, trait, or object to look up from. Objects must be written with a `.type` suffix, such as `Resource.at[SomeObject.type]`.
-    * @return A ResourceLoader for `T`.
+    * @return A Resource for `T`.
     * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]
     */
-  def at[T]: ResourceLoader =
+  def at[T]: Resource =
     macro Macros.atStaticImpl[T]
 
   /**
     * Look up class resource files.
     *
-    * This ResourceLoader looks up resources from the given Class, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `classOf[com.example.ExampleClass]` is given for `clazz`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
+    * This Resource looks up resources from the given Class, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `classOf[com.example.ExampleClass]` is given for `clazz`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
     *
     * If you want to look up resource files relative to the call site instead (that is, you want your class to look up one of its own resources), use the `my` method instead.
     *
@@ -90,36 +90,36 @@ object Resource extends ResourceLoader {
     *
     *         In this example, a file named `config.properties` is expected to appear alongside the file `AppClass.class` in the package `your`.
     * @param clazz The class to look up from.
-    * @return A ResourceLoader for `clazz`.
+    * @return A Resource for `clazz`.
     * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]
     */
-  def at(clazz: Class[_]): ResourceLoader =
+  def at(clazz: Class[_]): Resource =
     macro Macros.atDynamicImpl
 
   /**
     * Look up own resource files.
     *
-    * This ResourceLoader looks up resources from the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]] surrounding the call, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `my` is called from `com.example.ExampleClass`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
+    * This Resource looks up resources from the [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html Class]] surrounding the call, using [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]. For example, if `my` is called from `com.example.ExampleClass`, then resource files will be searched for in the `com/example` folder containing `ExampleClass.class`.
     *
     * @example {{{ Resource.my.url("config.properties") }}}
-    * @return A ResourceLoader for the call site.
+    * @return A Resource for the call site.
     * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/Class.html#getResource(java.lang.String) Class#getResource]]
     */
-  def my: ResourceLoader =
+  def my: Resource =
     macro Macros.myImpl
 
   /**
     * Look up resource files using the specified ClassLoader.
     *
-    * This ResourceLoader looks up resources from a specific ClassLoader. Like [[Resource the default ResourceLoader]], resource names are relative to the root package.
+    * This Resource looks up resources from a specific ClassLoader. Like [[Resource the default Resource]], resource names are relative to the root package.
     *
     * @example {{{ Resource.from(appClassLoader).url("com/example/config.properties") }}}
     * @param cl ClassLoader to look up resources from.
-    * @return A ResourceLoader that uses the supplied ClassLoader.
+    * @return A Resource that uses the supplied ClassLoader.
     * @see [[https://docs.oracle.com/javase/10/docs/api/java/lang/ClassLoader.html#getResource(java.lang.String) ClassLoader#getResource]]
     */
-  def from(cl: ClassLoader): ResourceLoader =
-    new ResourceLoader {
+  def from(cl: ClassLoader): Resource =
+    new Resource {
       override def url(name: String): Option[URL] =
         Option(cl.getResource(name))
     }
@@ -131,7 +131,7 @@ object Resource extends ResourceLoader {
     import c.universe._
     import c.Expr
 
-    def atStaticImpl[T](implicit T: WeakTypeTag[T]): Expr[ResourceLoader] = {
+    def atStaticImpl[T](implicit T: WeakTypeTag[T]): Expr[Resource] = {
       val rtc = Expr[Class[_]] {
         try {
           c.reifyRuntimeClass(T.tpe, concrete = true)
@@ -142,14 +142,14 @@ object Resource extends ResourceLoader {
       atDynamicImpl(rtc)
     }
 
-    def atDynamicImpl(clazz: Expr[Class[_]]): Expr[ResourceLoader] =
+    def atDynamicImpl(clazz: Expr[Class[_]]): Expr[Resource] =
       reify {
-        new ResourceLoader {
+        new Resource {
           override def url(name: String) = Option(clazz.splice.getResource(name))
         }
       }
 
-    def myImpl: Expr[ResourceLoader] = {
+    def myImpl: Expr[Resource] = {
       val rtc = c.reifyEnclosingRuntimeClass
       if (rtc.isEmpty) {
         // The documentation for reifyEnclosingRuntimeClass claims that this is somehow possible!?
