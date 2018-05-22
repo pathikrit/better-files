aec49e4762e6179ca89f08f87c7c676af924baec
pathikrit <pathikritbhowmick@msn.com>
2018-05-21 23:01:45 -0400

    Add multiple deploys .travis.yml

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sbt updateImpactSubmit coverageReport coverageAggregate codacyCoverage
-bash <(curl -s https://codecov.io/bash)
-git config --global user.email "pathikritbhowmick@msn.com"
-git config --global user.name "travis-ci"
-git config --global push.default simple
-sbt ghpagesPushSite +publish
