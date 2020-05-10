c9a8f48ef7a8930e4c24b954ebabf5e8299a5bf9
Scala Steward <me@scala-steward.org>
2020-05-10 16:17:39 +0200

    Update scalatest to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   def scalaReflect(version: String) = "org.scala-lang"     % "scala-reflect" % version % Provided
   val akka                          = "com.typesafe.akka" %% "akka-actor"    % "2.5.31" // Used in Akka file watcher
-  val scalatest                     = "org.scalatest"     %% "scalatest"     % "3.1.1" % Test
+  val scalatest                     = "org.scalatest"     %% "scalatest"     % "3.1.2" % Test
   val shapeless                     = "com.chuusai"       %% "shapeless"     % "2.3.3" % Test // For shapeless based Reader/Scanner in tests
   val commonsio                     = "commons-io"         % "commons-io"    % "2.6"   % Test // Benchmarks
   val fastjavaio =
