8ca4145aa37a703c1fc8b39a0f8755754f595d70
Scala Steward <me@scala-steward.org>
2020-05-28 06:36:40 +0200

    Update commons-io to 2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akka                          = "com.typesafe.akka" %% "akka-actor"    % "2.5.31" // Used in Akka file watcher
   val scalatest                     = "org.scalatest"     %% "scalatest"     % "3.1.1" % Test
   val shapeless                     = "com.chuusai"       %% "shapeless"     % "2.3.3" % Test // For shapeless based Reader/Scanner in tests
-  val commonsio                     = "commons-io"         % "commons-io"    % "2.6"   % Test // Benchmarks
+  val commonsio                     = "commons-io"         % "commons-io"    % "2.7"   % Test // Benchmarks
   val fastjavaio =
     "fastjavaio" % "fastjavaio" % "1.0" % Test from "https://github.com/williamfiset/FastJavaIO/releases/download/v1.0/fastjavaio.jar" //Benchmarks
 }
